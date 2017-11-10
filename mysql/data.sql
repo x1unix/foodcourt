c7ce5bb19d08d6948d6a4ac691ff211ecb389b8b
dsedchenko <dsedchenko@llnw.com>
2017-11-10 03:52:22 +0200

    Modify schema dumps

--- mysql/data.sql
+++ mysql/data.sql
@@ -1,73 +1,65 @@
--- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: foodcourt
--- ------------------------------------------------------
--- Server version	5.6.38
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4135
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.6.38)
+# Database: foodcourt
+# Generation Time: 2017-11-09 19:29:07 +0000
+# ************************************************************
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `dishes`
---
+
+# Dump of table dishes
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `dishes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `dishes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `label` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
-  `description` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `type` int(1) NOT NULL,
-  `photo_url` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `type` int(1) NOT NULL DEFAULT 0,
+  `photo_url` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `dish_id_UNIQUE` (`id`),
   UNIQUE KEY `label_UNIQUE` (`label`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `dishes`
---
 
 LOCK TABLES `dishes` WRITE;
 /*!40000 ALTER TABLE `dishes` DISABLE KEYS */;
-INSERT INTO `dishes` VALUES (1,'Vomit cake','just a cake, bro',0,NULL);
+
+INSERT INTO `dishes` (`id`, `label`, `description`, `type`, `photo_url`)
+VALUES
+	(1,'Vomit cake','just a cake, bro',0,'');
+
 /*!40000 ALTER TABLE `dishes` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`%`*/ /*!50003 TRIGGER `dishes_autoremove_menu_item` AFTER DELETE ON `dishes` FOR EACH ROW BEGIN
+/*!50003 SET SESSION SQL_MODE="NO_ENGINE_SUBSTITUTION" */;;
+/*!50003 CREATE */ /*!50017 DEFINER=`root`@`%` */ /*!50003 TRIGGER `dishes_autoremove_menu_item` AFTER DELETE ON `dishes` FOR EACH ROW BEGIN
  DELETE FROM menu WHERE menu.dish_id = OLD.id;
 END */;;
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET SESSION SQL_MODE=@OLD_SQL_MODE */;
 
---
--- Table structure for table `menu`
---
+
+# Dump of table menu
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `menu`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `menu` (
   `row_id` int(11) NOT NULL AUTO_INCREMENT,
   `dish_id` int(11) NOT NULL,
@@ -77,42 +69,31 @@ CREATE TABLE `menu` (
   KEY `dish_id` (`dish_id`),
   CONSTRAINT `menu_ibfk_1` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `menu`
---
 
 LOCK TABLES `menu` WRITE;
 /*!40000 ALTER TABLE `menu` DISABLE KEYS */;
-INSERT INTO `menu` VALUES (1,1,1509238390);
+
+INSERT INTO `menu` (`row_id`, `dish_id`, `date`)
+VALUES
+	(1,1,1509238390);
+
 /*!40000 ALTER TABLE `menu` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`%`*/ /*!50003 TRIGGER `menu_remove_order_on_delete` AFTER DELETE ON `menu` FOR EACH ROW BEGIN
+/*!50003 SET SESSION SQL_MODE="NO_ENGINE_SUBSTITUTION" */;;
+/*!50003 CREATE */ /*!50017 DEFINER=`root`@`%` */ /*!50003 TRIGGER `menu_remove_order_on_delete` AFTER DELETE ON `menu` FOR EACH ROW BEGIN
  DELETE FROM orders WHERE orders.item_id = OLD.row_id;
 END */;;
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET SESSION SQL_MODE=@OLD_SQL_MODE */;
 
---
--- Table structure for table `orders`
---
+
+# Dump of table orders
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `orders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `orders` (
   `order_id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) NOT NULL,
@@ -124,58 +105,23 @@ CREATE TABLE `orders` (
   CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `menu` (`row_id`),
   CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders`
---
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (2,1,1);
+
+INSERT INTO `orders` (`order_id`, `item_id`, `user_id`)
+VALUES
+	(2,1,1);
+
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`%`*/ /*!50003 TRIGGER `update_stats_on_order_insert` AFTER INSERT ON `orders` FOR EACH ROW BEGIN
- 
- SET @sel_dish_id = (SELECT `dish_id` from `menu` WHERE `row_id` = NEW.item_id);
 
- 
- SET @stat_has_item = (SELECT COUNT(*) FROM `stats` WHERE `stats`.`dish_id` = @sel_dish_id AND `stats`.`user_id` = NEW.user_id);
- 
- IF @stat_has_item = 0 THEN
-  
-  SET @stat_count = (SELECT `hit_count` FROM `stats` WHERE `stats`.`dish_id` = @sel_dish_id AND `stats`.`user_id` = NEW.user_id);
 
-  
-  UPDATE `stats` SET `hit_count` = @stat_count+1;
- ELSE
-  
-  INSERT INTO `stats` (`dish_id`, `user_id`, `hit_count`) values (@sel_dish_id, NEW.user_id, 1);
- END IF;
-
-END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-
---
--- Table structure for table `ratings`
---
+# Dump of table ratings
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `ratings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `ratings` (
   `row_id` int(11) NOT NULL AUTO_INCREMENT,
   `dish_id` int(11) NOT NULL,
@@ -189,63 +135,21 @@ CREATE TABLE `ratings` (
   CONSTRAINT `ratings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `ratings_ibfk_2` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `ratings`
---
 
-LOCK TABLES `ratings` WRITE;
-/*!40000 ALTER TABLE `ratings` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ratings` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `stats`
---
-
-DROP TABLE IF EXISTS `stats`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `stats` (
-  `stat_id` int(33) NOT NULL AUTO_INCREMENT,
-  `dish_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `hit_count` int(11) DEFAULT '0',
-  PRIMARY KEY (`stat_id`),
-  UNIQUE KEY `stat_id_UNIQUE` (`stat_id`),
-  UNIQUE KEY `dish_id_UNIQUE` (`dish_id`),
-  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
-  KEY `dish_id` (`dish_id`),
-  KEY `hit_count` (`hit_count`),
-  CONSTRAINT `stats_ibfk_1` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`),
-  CONSTRAINT `stats_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `stats`
---
-
-LOCK TABLES `stats` WRITE;
-/*!40000 ALTER TABLE `stats` DISABLE KEYS */;
-/*!40000 ALTER TABLE `stats` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
+# Dump of table users
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
   `firstName` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
   `lastName` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
   `password` varchar(64) CHARACTER SET utf8 NOT NULL,
-  `level` int(1) NOT NULL DEFAULT '2',
+  `level` int(1) NOT NULL DEFAULT 2,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
@@ -253,67 +157,34 @@ CREATE TABLE `users` (
   KEY `id` (`id`),
   KEY `password` (`password`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin@llnw.net','Lorem','Ipsum','7cbb3252ba6b7e9c422fac5334d22054',0);
+
+INSERT INTO `users` (`id`, `email`, `firstName`, `lastName`, `password`, `level`)
+VALUES
+	(1,'admin@llnw.com','Lorem','Ipsum','7cbb3252ba6b7e9c422fac5334d22054',0);
+
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+
 DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`%`*/ /*!50003 TRIGGER `users_crypt_pass` BEFORE INSERT ON `users` FOR EACH ROW BEGIN
+/*!50003 SET SESSION SQL_MODE="NO_ENGINE_SUBSTITUTION" */;;
+/*!50003 CREATE */ /*!50017 DEFINER=`root`@`%` */ /*!50003 TRIGGER `users_crypt_pass` BEFORE INSERT ON `users` FOR EACH ROW BEGIN
  SET NEW.password = md5(NEW.password);
 END */;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`%`*/ /*!50003 TRIGGER `users_on_update_crypt_pass` BEFORE UPDATE ON `users` FOR EACH ROW BEGIN
+/*!50003 SET SESSION SQL_MODE="NO_ENGINE_SUBSTITUTION" */;;
+/*!50003 CREATE */ /*!50017 DEFINER=`root`@`%` */ /*!50003 TRIGGER `users_on_update_crypt_pass` BEFORE UPDATE ON `users` FOR EACH ROW BEGIN
     SET NEW.password = md5(NEW.password);
   END */;;
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET SESSION SQL_MODE=@OLD_SQL_MODE */;
 
---
--- Dumping events for database 'foodcourt'
---
 
---
--- Dumping routines for database 'foodcourt'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-11-07 20:58:36
