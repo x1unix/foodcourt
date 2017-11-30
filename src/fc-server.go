86aae8ccced6c64011c9f5555a0466d394f1acb5
dsedchenko <dsedchenko@llnw.com>
2017-12-01 00:34:21 +0200

    fix(main): set default server log name

--- src/fc-server.go
+++ src/fc-server.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	config.Bootstrap()
 
-	log := logger.Bootstrap("fc-server")
+	log := logger.Bootstrap("fc")
 	router := route.Bootstrap()
 
 	httpPort := config.Get(config.HTTP_PORT, "80")
