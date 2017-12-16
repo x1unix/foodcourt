aab02c1b01f9e09c40210b9c7df19a5422597be8
dsedchenko <dsedchenko@llnw.com>
2017-12-16 23:53:32 +0200

    fix(ui): Fix error message extractor

--- src/ui/src/app/shared/services/web-helper.service.ts
+++ src/ui/src/app/shared/services/web-helper.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpErrorResponse} from '@angular/common/http';
-import { isObject, isString, isNil } from 'lodash';
+import {isNil, isObject, isString} from 'lodash';
 import {IMessage} from '../interfaces/message';
 
 const ERR_DEFAULT = 'failed to perform request to API service';
@@ -20,12 +20,12 @@ export class WebHelperService {
       return err.error;
     }
 
-    if (isObject(err.error)) {
+    if (isObject(err.error) && isObject(err.error.msg)) {
       const msg: IMessage = <IMessage> err.error;
       return `${msg.msg} (${err.status} ${err.statusText})`;
     }
 
-    return `${err.status} ${err.statusText || ERR_DEFAULT}`;
+    return `${err.message} (${err.status} ${err.statusText || ERR_DEFAULT})`;
   }
 
 }
